54cb26d09b9a6b2d52f719111cc5f938d858aeb4
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-24 19:15:36 +0100

    App update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,31 @@
 import './App.css';
 import Sticker from './components/Sticker';
 import Model from './components/Model';
+import Box from './components/Box';
+
+import { Canvas } from "@react-three/fiber";
+import Cylinder3d from "./components/Cylinder";
 
 function App() {
+	
 
 
 	return (
 		<div className="App">
 			<Sticker />
-			<Model />
+			<Box>
+				<Model />
+			</Box>
+			
+				
+			{/* <section className='App-header'>
+				<Canvas>
+				<pointLight position={[10, 10, 10]} /> 
+				<ambientLight />
+				<Cylinder3d position={[-1.2, 0, 0]} wireframe={false} />
+				<Cylinder3d position={[1.2, 0, 0]} wireframe={false} />
+				</Canvas>
+			</section> */}
 		</div>
 	);
 }
