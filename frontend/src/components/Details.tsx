84b7787cdc86d54f8ea12858ded45f0269a969eb
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-25 15:11:53 +0100

    Details module values

--- frontend/src/components/Details.tsx
+++ frontend/src/components/Details.tsx
@@ -5,15 +5,15 @@ const Details: React.FC<{code: string}> = (props) => {
     return <div className={classes.main}>
         <div className={classes.dark}>
             <p className={classes.name}>Width</p>
-            <p className={classes.number}>42</p>
+            <p className={classes.number}>120</p>
         </div>
         <div className={classes.light}>
             <p className={classes.name}>Height</p>
-            <p className={classes.number}>84</p>
+            <p className={classes.number}>85</p>
         </div>
         <div className={classes.dark}>
             <p className={classes.name}>Depth</p>
-            <p className={classes.number}>55</p>
+            <p className={classes.number}>60</p>
         </div>
         <div className={classes.light}>
             <p className={classes.name}>Product code</p>
