1a73341263521368fb2f250a42747d592e28c0c3
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-25 17:18:34 +0100

    texture buttons

--- frontend/src/components/TextureButton.tsx
+++ frontend/src/components/TextureButton.tsx
@@ -2,11 +2,11 @@ import classes from './TextureButton.module.css';
 
 const TextureButton = () => {
     return <div className={classes.grid}>
-        <button className={`${classes.texture} ${classes.brown}`} title='Brown'></button>
-        <button className={`${classes.texture} ${classes.grey}`} title='Grey'></button>
-        <button className={`${classes.texture} ${classes.white}`} title='White'></button>
-        <button className={`${classes.texture} ${classes.orange}`} title='Orange'></button>
-        <button className={`${classes.texture} ${classes.purple}`} title='Purple'></button>
+        <button id='brown' className={`${classes.texture} ${classes.brown}`} title='Brown'></button>
+        <button id='grey' className={`${classes.texture} ${classes.grey}`} title='Grey'></button>
+        <button id='white' className={`${classes.texture} ${classes.white}`} title='White'></button>
+        <button id='orange' className={`${classes.texture} ${classes.orange}`} title='Orange'></button>
+        <button id='purple' className={`${classes.texture} ${classes.purple}`} title='Purple'></button>
     </div>
 };
 
