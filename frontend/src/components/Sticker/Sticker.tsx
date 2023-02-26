bf9220ecc79c178ee88b3b58cb5866ee6551425d
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-26 19:16:37 +0100

    Sticker upda6te

--- frontend/src/components/Sticker/Sticker.tsx
+++ frontend/src/components/Sticker/Sticker.tsx
@@ -1,14 +1,52 @@
 import classes from './Sticker.module.css';
 import '../fontello/css/fontello.css';
 
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
 const Sticker = () => {
+
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+    interface CartState {
+        cart: { products: { quantity: number, variant: string }[], isEmpty: boolean, totalItems: number }
+    }
+
+    const cartSelector = (state: CartState) => state.cart;
+
+    const totalCartItems = useSelector(cartSelector).totalItems;
+    const isCartEmpty = useSelector(cartSelector).isEmpty;
+
+
+    const btnClasses = `${classes['cartQuantity']} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (isCartEmpty) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [totalCartItems]);
+
+
     return (<div className={classes.header}>
         <h1>TabConfig</h1>
-            <div className={`${classes['font-basket']}`}>
-                <p>Shop Cart</p>
-                <i className="icon-basket" style={{ fontSize: '32px'}} />
-                {/* <i className="icon-basket" style={{ fontSize: '20px' , marginRight: '10px'}} /> */}
-            </div>
+        <div className={`${classes['font-basket']}`}>
+            <p>Shop Cart</p>
+            <i className="icon-basket" style={{ fontSize: '32px' }} />
+            {!isCartEmpty && 
+            <div className={btnClasses}>
+                <p className={classes.p}>{totalCartItems}</p>
+            </div>}
+            {/* <i className="icon-basket" style={{ fontSize: '20px' , marginRight: '10px'}} /> */}
+        </div>
     </div>);
 };
 
