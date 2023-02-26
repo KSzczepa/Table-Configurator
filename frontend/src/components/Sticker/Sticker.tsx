f1850160ff2a832c6c6f919d717b05eb1e0cb657
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-26 15:25:24 +0100

    Sticker
    styling changes

--- frontend/src/components/Sticker/Sticker.tsx
+++ frontend/src/components/Sticker/Sticker.tsx
@@ -1,8 +1,14 @@
 import classes from './Sticker.module.css';
+import '../fontello/css/fontello.css';
 
 const Sticker = () => {
     return (<div className={classes.header}>
         <h1>TabConfig</h1>
+            <div className={`${classes['font-basket']}`}>
+                <p>Shop Cart</p>
+                <i className="icon-basket" style={{ fontSize: '32px'}} />
+                {/* <i className="icon-basket" style={{ fontSize: '20px' , marginRight: '10px'}} /> */}
+            </div>
     </div>);
 };
 
