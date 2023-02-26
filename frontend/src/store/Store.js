f21ef27224f2cea262870c323fc4ff266c2b9ade
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-26 17:44:01 +0100

    Store update
    Counter for items moved to other slice

--- frontend/src/store/Store.js
+++ frontend/src/store/Store.js
@@ -1,12 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 
-import counterReducer from './counter';
 import sceneReducer from './scene';
 import cartReducer from './cart';
 
 const store = configureStore({
     reducer: {
-        counter: counterReducer,
         product: sceneReducer,
         cart: cartReducer
     }
