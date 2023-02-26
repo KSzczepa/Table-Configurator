4f95fb2850fdcb63509fa6d947ee6e1a4b4c2ffe
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-26 15:25:34 +0100

    Store update

--- frontend/src/store/Store.js
+++ frontend/src/store/Store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import counterReducer from './counter';
 import sceneReducer from './scene';
+import cartReducer from './cart';
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         scene: sceneReducer,
+        cart: cartReducer
     }
 });
 
