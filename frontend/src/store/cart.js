c9947f8e533541f92648fe73874b9dc6677d5a86
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-26 17:41:12 +0100

    cart update
    Basket logic update in redux

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,19 +1,37 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialCartState = {quantity: 0, variant: ''};
+const initialCartState = {products: [], isEmpty: true, totalItems: 0};
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: initialCartState,
     reducers: {
         addProductToCart(state, action) {
-            // state.counter++;
+
+            if (!state.isEmpty) {
+                const finded = state.products.findIndex(val => val.variant === action.payload.variant);
+                if (finded != -1) {
+                    state.products[finded].quantity += action.payload.quantity;
+                    state.totalItems += action.payload.quantity;
+                    return;
+                }
+            }
+            
+            
+            state.products.push(action.payload);
+            state.totalItems += action.payload.quantity;
+            state.isEmpty = false;
+            
+
         },
         deleteProductFromCart(state) {
             
         },
         sendProductToDB(state, action) {
             // state.counter = state.counter + action.payload;
+        },
+        setQuantityToDefault(state) {
+
         },
     }
 });
