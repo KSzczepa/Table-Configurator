c1a8603228bab51c4e2e4f7be804698538ac41e4
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-03-03 21:52:09 +0100

    Root layout update

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -5,13 +5,31 @@ import Model from '../components/Model';
 import Box from '../components/Box';
 import ItemHandlerBox from '../components/descriptionPart/ItemHandlerBox';
 import Options from '../components/textureOptions/Options';
+import Cart from '../components/Cart/Cart';
+import { useSelector, useDispatch } from 'react-redux';
+import { cartActions } from '../store/cart';
 
 const RootLayout = () => {
     // const navigation = useNavigation();
 
+    const dispatch = useDispatch();
+
+    interface CartState {
+        cart: { isCartVisible: boolean }
+    }
+
+    const cartSelector = (state: CartState) => state.cart;
+
+    const isCartVisible = useSelector(cartSelector).isCartVisible;
+
+    const onCloseCart = () => {
+        dispatch(cartActions.onCloseCart());
+    }
+
     return <Fragment>
         <Sticker />
         <main>
+            {isCartVisible && <Cart/>}
             <Box>
 				<Model />
 			</Box>
