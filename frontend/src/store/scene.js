5709d31f7b6149fb805be718d27e4808f42d4286
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-03-04 12:06:34 +0100

    Scene update

--- frontend/src/store/scene.js
+++ frontend/src/store/scene.js
@@ -2,7 +2,35 @@ import {createSlice} from '@reduxjs/toolkit';
 import Texture from '../assets/Textures';
 import { modelScene as scene } from '../assets/Scene';
 
-const initialProductState = {texture: 'brown', itemsCounter: 1};
+const DUMMY_VALUES = [
+    {
+        texture: 'brown',
+        prodCode: 'TAB-BR-K5',
+        price: 199.99,
+    },
+    {
+        texture: 'grey',
+        prodCode: 'TAB-GR-K5',
+        price: 210.99,
+    },
+    {
+        texture: 'white',
+        prodCode: 'TAB-WH-K5',
+        price: 314.99,
+    },
+    {
+        texture: 'orange',
+        prodCode: 'TAB-OR-K5',
+        price: 269.99,
+    },
+    {
+        texture: 'purple',
+        prodCode: 'TAB-PU-K5',
+        price: 349.99,
+    },
+]
+
+const initialProductState = {texture: 'brown', itemsCounter: 1, price: 199.99, code: 'TAB-BR-K5'};
 
 const productSlice = createSlice({
     name: 'product',
@@ -11,6 +39,9 @@ const productSlice = createSlice({
         changeTexture(state, action) {
             state.texture = action.payload;
             Texture.LoadTexture(scene, action.payload);
+            const finded = DUMMY_VALUES.findIndex(val => val.texture === state.texture);
+            state.code = DUMMY_VALUES[finded].prodCode;
+            state.price = DUMMY_VALUES[finded].price;
         },
         increment(state) {
             state.itemsCounter++;
