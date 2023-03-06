46fdae8bf44459334efe9d12298de1323193b874
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-03-06 21:42:38 +0100

    Cart module update
    Added styling to basket

--- frontend/src/components/Cart/Cart.tsx
+++ frontend/src/components/Cart/Cart.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import classes from './Cart.module.css';
 
 import Card from "../UI/Card";
+import ImageLoader from "./ImageLoader";
 
 const Cart = () => {
 
@@ -43,12 +44,19 @@ const Cart = () => {
 
     const productsInCart = (products.map(item =>             
         <Card key={item.code}>   
-            <div>       
+            <div className={classes.picture}>
+                <ImageLoader variant={item.variant}/>
+            </div>
+            <div className={classes.description}>       
                 <span>Amazing wooden table</span><br/>
                 <span>Color: {item.variant}</span><br/>
             </div>  
-            <div>
-                <button onClick={decreaseProdQuantity} id={item.variant}>-</button><span> {item.quantity} </span><button onClick={increaseProdQuantity} id={item.variant}>+</button><br />
+            <div className={classes.value}>
+                <div className={classes.quantity}>
+                    <button onClick={decreaseProdQuantity} id={item.variant}>-</button>
+                    <span> {item.quantity} </span>
+                    <button onClick={increaseProdQuantity} id={item.variant}>+</button><br />
+                </div>
                 <span>{item.price} PLN</span>
             </div>
         </Card>
@@ -58,7 +66,7 @@ const Cart = () => {
     const CartContent = <div className = {classes.cartContent}>
         <div className={classes.header}><h2>Your products</h2><h4>{totalItems} items</h4></div>
         {totalItems > 0 ? productsInCart : <p>Your shopping cart is empty!</p>}
-        {totalItems > 0 ? <h3>Total: {totalPrice} PLN</h3> : ''}        
+        {totalItems > 0 ? <div className={classes.summary}><h3>Total: {totalPrice} PLN</h3><button>Order</button></div> : ''}        
     </div>
 
     return <Modal onClose={onCloseCart}>
