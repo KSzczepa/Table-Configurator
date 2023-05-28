7898e7ba06f3f35971e60028f45dca4250b0dd8d
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-05-28 11:34:14 +0200

    Product Spec

--- frontend/src/components/productDetails/ProductSpecsMainComponent/ProductSpecs.tsx
+++ frontend/src/components/productDetails/ProductSpecsMainComponent/ProductSpecs.tsx
@@ -2,57 +2,66 @@ import AddProductButtons from '../UI/AddProductButtons';
 import ProductDimensionsDetails from '../ProductDimensionDetails/ProductDimensionsDetails';
 import classes from './ProductSpecs.module.css';
 import { useSelector } from 'react-redux';
+import { getData } from '../../../shared/getDataHandler';
+import { useState, useEffect, Fragment } from 'react';
 
-const DUMMY_VALUES = [
-    {
-        texture: 'brown',
-        prodCode: 'TAB-BR-K5',
-        price: 199.99,
-    },
-    {
-        texture: 'grey',
-        prodCode: 'TAB-GR-K5',
-        price: 210.99,
-    },
-    {
-        texture: 'white',
-        prodCode: 'TAB-WH-K5',
-        price: 314.99,
-    },
-    {
-        texture: 'orange',
-        prodCode: 'TAB-OR-K5',
-        price: 269.99,
-    },
-    {
-        texture: 'purple',
-        prodCode: 'TAB-PU-K5',
-        price: 349.99,
-    },
-]
+type productSpecification = {
+    texture: string,
+    prodCode: string,
+    price: number
+}
 
 const ProductSpecsBox = () => {
 
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
+    const [productDetails, setProductDetails] = useState({});
+    const [productCode, setProductCode] = useState('');
+    const [productPrice, setProducPrice] = useState(0);
+
     interface ProductState {
-        product: {texture: string, itemsCounter: number}
+        product: { texture: string, itemsCounter: number }
     }
 
     const productSelector = (state: ProductState) => state.product.texture;
 
     const productVariant: string = useSelector(productSelector);
 
+    const getProductsfromDB = async () => {
+        const response = await getData('https://tabletweak-default-rtdb.firebaseio.com/products.json');
+        setProductDetails(response);
+        return response;
+    }
 
-    const finded = DUMMY_VALUES.findIndex(val => val.texture === productVariant);
-    const productCode = DUMMY_VALUES[finded].prodCode;
-    const productPrice = DUMMY_VALUES[finded].price;
+    const setCurrentProductDescriptionVisible = () => {
+
+        const values = Object.values(productDetails) as productSpecification[];
+        console.log(values);
+        const finded = values.findIndex(val => val.texture === productVariant);
+        setProductCode(values[finded].prodCode);
+        setProducPrice(values[finded].price);
+    }
+
+
+    useEffect(() => {
+        if (Object.entries(productDetails).length === 0)
+            getProductsfromDB();
+        else {
+            if (!isDataLoaded)
+                setIsDataLoaded(true);
+            setCurrentProductDescriptionVisible();            
+        }
+    }, [productDetails, productVariant]);
 
 
     return <div className={classes.box}>
-        <h1 className={classes.title}>Amazing wooden table</h1>
-        <p>Model: Katrina5</p>
-        <p className={classes.price}>{productPrice + ' PLN'}</p>
-        <AddProductButtons />
-        <ProductDimensionsDetails code={productCode}/>
+        {isDataLoaded ? <Fragment>
+            <h1 className={classes.title}>Amazing wooden table</h1>
+            <p>Model: Katrina5</p>
+            <p className={classes.price}>{productPrice + ' PLN'}</p>
+            <AddProductButtons />
+            <ProductDimensionsDetails code={productCode} />
+        </Fragment> : ''}
+
     </div>
 }
 
